8b0d8190539dba84dbed81ceaeeebeeefb22dd91
RobinAdam747 <adam.sendzul@gmail.com>
2024-05-08 12:11:20 +0200

    added line to enable the USB connection to the setup file

--- devel/setup.bash
+++ devel/setup.bash
@@ -6,3 +6,6 @@ CATKIN_SHELL=bash
 # source setup.sh from same directory as this file
 _CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
 . "$_CATKIN_SETUP_DIR/setup.sh"
+
+# setup step for the usb serial connection to the Pioneer 3AT robot when running RosAria
+sudo chmod a+rw /dev/ttyUSB0
